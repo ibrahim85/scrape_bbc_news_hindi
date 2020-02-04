f2032c491e83db4d3e0c4f8329882250b474b1e2
ibrahim gad <hemagad@gmail.com>
2020-02-05 01:25:31 +0200

    Update pipelines.py

--- BBC_NewsHindi/pipelines.py
+++ BBC_NewsHindi/pipelines.py
@@ -6,6 +6,47 @@
 # See: http://doc.scrapy.org/en/latest/topics/item-pipeline.html
 
 
+
+from scrapy import signals
+from scrapy.exporters import XmlItemExporter
+
+
+# class TutorialPipeline(object):
+#     def process_item(self, item, spider):
+#         for field in item:
+#             print field + ': ' + item[field][0]
+#         return item
+    
 class BbcNewshindiPipeline(object):
     def process_item(self, item, spider):
         return item
+
+class XmlExportPipeline(object):
+
+    def __init__(self):
+        self.files = {}
+
+    @classmethod
+    def from_crawler(cls, crawler):
+        pipeline = cls()
+        crawler.signals.connect(pipeline.spider_opened, signals.spider_opened)
+        crawler.signals.connect(pipeline.close_spider, signals.spider_closed)
+        return pipeline
+
+    def spider_opened(self, spider):
+        file = open('%s_products.xml' % spider.name, 'w+b')
+        self.files[spider] = file
+        self.exporter = XmlItemExporter(file)
+        self.exporter.start_exporting()
+
+    def close_spider(self, spider):
+        self.exporter.finish_exporting()
+        file = self.files.pop(spider)
+        file.close()
+
+    def process_item(self, item, spider):
+        self.exporter.export_item(item)
+        if item.content_news is None:
+            pass
+        else:
+            return item
